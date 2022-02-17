a357ac7037cce2ad05d8e9764d5ec1150a698056
BinaryWiz <ultimi450@gmail.com>
2022-02-16 21:42:06 -0500

    started working on the landing page

---
+++ src/components/landing/introduction.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full flex flex-col lg:flex-row">
+    <div class="w-full lg:w-1/2 text-center">
+      <h1>Bide</h1>
+      <h2>It's Only a Matter of Time</h2>
+    </div>
+    <div class="w-full lg:w-1/2">
+      <h2>Join Bide!</h2>
+      <div class="flex flex-row">
+        <h3>ID:</h3>
+        <input type="text" />
+      </div>
+      <button>Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    console.log('here')
+  }
+})
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat'],
+        poppins: ['Poppins']
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
